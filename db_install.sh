9954c698e6251a7afb8a24367d7160a732d2d9e0
Diogo Tosta Silva <diogotosta_silva@hotmail.com>
2024-01-06 20:56:15 -0300

    initial commit

---
+++ db_install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sqlite3 moneyflow_manager.db < schema.sql
---
+++ schema.sql
@@ -0,0 +1,54 @@
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    hash TEXT NOT NULL,
+    salt TEXT NOT NULL
+);
+CREATE UNIQUE INDEX users_username ON users(username);
+
+CREATE TABLE transactions(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    ammount INTEGER INTEGER NOT NULL,
+    date TEXT NOT NULL,
+    currency TEXT NOT NULL,
+    category_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES transaction_categories(id)
+);
+CREATE INDEX transactions_user_id ON transactions(user_id);
+CREATE INDEX transactions_category_id ON transactions(category_id);
+
+CREATE TABLE transaction_categories(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    type TEXT NOT NULL,
+    name TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+CREATE INDEX transaction_categories_user_id ON transaction_categories(user_id);
+CREATE INDEX transaction_categories_type ON transaction_categories(type);
+
+CREATE TABLE exchanges(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    source_currency TEXT NOT NULL,
+    source_ammount INTEGER NOT NULL,
+    destination_currency TEXT NOT NULL,
+    destination_ammount INTEGER NOT NULL,
+    date TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+CREATE INDEX exchanges_source_currency ON exchanges(source_currency);
+CREATE INDEX exchanges_destination_currency ON exchanges(destination_currency);
+CREATE INDEX exchanges_user_id ON exchanges(user_id);
+
+-- Refers to how much a unit of 1 in the column currency is worth in USD
+CREATE TABLE historical_rates(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT NOT NULL,
+    BRL INTEGER,
+    EUR INTEGER,
+    GBP INTEGER
+);
+CREATE UNIQUE INDEX historical_rates_date ON historical_rates(date);
