d39edfbbe71152a277baa468e32edd971050ca31
Diogo Tosta Silva <diogotosta_silva@hotmail.com>
2024-01-17 17:49:35 -0300

    add deletion - transactions

--- blueprints/transactions.py
+++ blueprints/transactions.py
@@ -18,3 +18,21 @@ def transactions():
     return render_template("transactions.html", results=results, has_date=True,
                            add_url="/transactions/forms/add", title="Transactions",
                            add_btn_txt="Add new transaction")
+
+
+@transactions_bp.post("/transactions/remove")
+def remove_transaction():
+    ids = request.json["id"]
+    if len(ids) == 0:
+        return {"status" : "fail", "message" : "No rows selected"}
+    
+    with db_engine.begin() as conn:
+        query = delete(transactions_table).where(
+            and_(transactions_table.c.id.in_(ids), transactions_table.c.user_id == session["user_id"]))
+
+        deleted_rows_count = conn.execute(query).rowcount
+        if len(ids) != deleted_rows_count:
+            conn.rollback()
+            return {"status" : "fail", "message" : "An error has occurred"}
+        
+    return {"status" : "success", "message" : "Rows deleted successfully"}
