de270c63a6da27457d52e5bf9197b8737d62af99
Diogo Tosta Silva <diogotosta_silva@hotmail.com>
2024-01-20 04:10:16 -0300

    add exchange update feat

--- blueprints/exchanges.py
+++ blueprints/exchanges.py
@@ -42,13 +42,9 @@ def add_exchange():
     source_ammount = convert_money_input_to_db(request.form.get("source_ammount"))
     destination_ammount = convert_money_input_to_db(request.form.get("destination_ammount"))
     
-    if not (date and source_currency and destination_currency and source_ammount != None and destination_ammount != None):
-        return {"status" : "fail", "message" : "Blank fields"}
-    
-    if source_ammount <= 0 or destination_ammount <= 0:
-        return {"status" : "fail", "message" : "Non-positive ammount"}
-    if not (destination_currency in ["BRL", "USD", "EUR", "GBP"] and source_currency in ["BRL", "USD", "EUR", "GBP"]):
-        return {"status" : "fail", "message" : "Invalid currency"}
+    situation = validate_exchange_input(date, source_currency, destination_currency, source_ammount, destination_ammount)
+    if situation["status"] == "fail":
+        return situation
     
     with db_engine.begin() as conn:
         query = insert(exchanges_table).values(
@@ -58,4 +54,55 @@ def add_exchange():
             )
         conn.execute(query)
 
-    return {"status" : "success", "message" : "exchange successfully added"}
+    return {"status" : "success", "message" : "Exchange successfully added"}
+
+
+@exchanges_bp.post("/exchanges/forms/update")
+def update_exchange_form():
+    id = request.form.get("id")
+    date = db_date_to_html(request.form.get("date"))
+    source_ammount = request.form.get("source_ammount")
+    source_currency = request.form.get("source_currency")
+    destination_ammount = request.form.get("destination_ammount")
+    destination_currency = request.form.get("destination_currency")
+    return render_template("update_exchange.html", id=id, source_ammount=source_ammount,
+                           source_currency=source_currency,destination_ammount=destination_ammount,
+                           destination_currency=destination_currency, date=date, title="Update Exchange",
+                           form_title="Update Exchange", styles=["/static/exchanges_forms.css"])
+
+
+@exchanges_bp.post("/exchanges/update")
+def update_exchange():
+    id = request.form.get("id")
+    date = html_date_to_db(request.form.get("date"))
+    source_ammount = convert_money_input_to_db(request.form.get("source_ammount"))
+    source_currency = request.form.get("source_currency")
+    destination_ammount = convert_money_input_to_db(request.form.get("destination_ammount"))
+    destination_currency = request.form.get("destination_currency")
+
+    situation = validate_exchange_input(date, source_currency, destination_currency, source_ammount, destination_ammount)
+    if situation["status"] == "fail":
+        return situation
+    
+    with db_engine.begin() as conn:
+        query = update(exchanges_table).values(date=date, source_ammount=source_ammount,
+                source_currency=source_currency, destination_ammount=destination_ammount,
+                destination_currency=destination_currency).where(
+                    and_(exchanges_table.c.user_id == session["user_id"], exchanges_table.c.id == id)
+                )
+        count = conn.execute(query).rowcount
+        if count < 1:
+            return {"status" : "fail", "message" : "An error has occured"}
+
+    return {"status" : "success", "message" : "Exchange successfully updated"}
+
+
+def validate_exchange_input(date, source_currency, destination_currency, source_ammount, destination_ammount):
+    if not (date and source_currency and destination_currency and source_ammount != None and destination_ammount != None):
+        return {"status" : "fail", "message" : "Blank fields"}
+    
+    if source_ammount <= 0 or destination_ammount <= 0:
+        return {"status" : "fail", "message" : "Non-positive ammount"}
+    if not (destination_currency in ["BRL", "USD", "EUR", "GBP"] and source_currency in ["BRL", "USD", "EUR", "GBP"]):
+        return {"status" : "fail", "message" : "Invalid currency"}
+    return {"status" : "success"}
