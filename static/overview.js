aebccd62c69fcdb704890ce81ee9316f248e6ab0
Diogo Tosta Silva <diogotosta_silva@hotmail.com>
2024-01-24 00:57:09 -0300

    add responsiveness and exchange rate impact to frontend

--- static/overview.js
+++ static/overview.js
@@ -3,12 +3,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
 });
 
 async function initialize_page(){
+    document.querySelector(".exchange-rate-impact-style").style.display = "none";
+
     let income_canvas = document.getElementById("income_canvas");
     let expenses_canvas = document.getElementById("expenses_canvas");
     let total_expenses = document.getElementById("total_expenses");
     let total_income = document.getElementById("total_income");
     let total = document.getElementById("total");
     let dropdown = document.getElementById("view_dropdown");
+
+    let exchange_rate_impact = document.getElementById("exchange_rate_impact");
     
     if(dropdown.value == ""){
         total.innerHTML = "0.00";
@@ -18,13 +22,13 @@ async function initialize_page(){
         return;
     }
 
-    let charts = await initialize_display(income_canvas, expenses_canvas, total, total_expenses, total_income, dropdown);
+    let charts = await initialize_display(income_canvas, expenses_canvas, total, total_expenses, total_income, exchange_rate_impact, dropdown);
     dropdown.addEventListener("change", ()=>{
-        update_chart(charts[0], charts[1], total, total_expenses, total_income, dropdown);
+        update_chart(charts[0], charts[1], total, total_expenses, total_income, exchange_rate_impact ,dropdown);
     });
 }
 
-async function update_chart(income_chart, expenses_chart, total, total_expenses, total_income, dropdown){
+async function update_chart(income_chart, expenses_chart, total, total_expenses, total_income, exchange_rate_impact ,dropdown){
     let overview_data = await fetch_overview(dropdown);
     income_chart.data.labels = overview_data["income_labels"];
     income_chart.data.datasets[0].data = overview_data["income_data"];
@@ -34,7 +38,7 @@ async function update_chart(income_chart, expenses_chart, total, total_expenses,
     expenses_chart.data.datasets[0].data = overview_data["expenses_data"];
     expenses_chart.update();
 
-    set_totals(total, total_expenses, total_income, overview_data);
+    set_totals(total, total_expenses, total_income, exchange_rate_impact, overview_data);
 }
 
 async function fetch_overview(dropdown){
@@ -51,7 +55,7 @@ async function fetch_overview(dropdown){
     return await response.json();
 }
 
-async function initialize_display(income_canvas, expenses_canvas, total, total_expenses, total_income, dropdown){
+async function initialize_display(income_canvas, expenses_canvas, total, total_expenses, total_income, exchange_rate_impact, dropdown){
     let overview_data = await fetch_overview(dropdown);
 
     let chart_colors = [
@@ -65,22 +69,27 @@ async function initialize_display(income_canvas, expenses_canvas, total, total_e
         '#FF3131'
     ]
 
-    console.log(overview_data["income_labels"]);
-    console.log(overview_data["income_data"]);
     let income_chart = new_doughnut_chart(income_canvas, overview_data["income_labels"], overview_data["income_data"], "Income", chart_colors);
     let expenses_chart = new_doughnut_chart(expenses_canvas, overview_data["expenses_labels"], overview_data["expenses_data"], "Expenses", chart_colors);
 
-    set_totals(total, total_expenses, total_income, overview_data);
+    set_totals(total, total_expenses, total_income, exchange_rate_impact, overview_data);
 
     document.getElementById("total_str").style.visibility = "visible";
 
     return [income_chart, expenses_chart];
 }
 
-function set_totals(total, total_expenses, total_income, overview_data){
+function set_totals(total, total_expenses, total_income, exchange_rate_impact, overview_data){
     total.innerHTML = overview_data["total"];
     total_expenses.innerHTML = overview_data["total_expenses"];
     total_income.innerHTML = overview_data["total_income"];
+    if (overview_data.hasOwnProperty("exchange_rate_impact")){
+        document.querySelector(".exchange-rate-impact-style").style.display = "block";
+        exchange_rate_impact.innerHTML = overview_data["exchange_rate_impact"];
+    }
+    else{
+        document.querySelector(".exchange-rate-impact-style").style.display = "none";
+    }
 }
 
 function new_doughnut_chart(canvas, labels, dataset_data, dataset_label, colors){
@@ -96,6 +105,9 @@ function new_doughnut_chart(canvas, labels, dataset_data, dataset_label, colors)
 
     return ret_chart = new Chart(canvas, {
         type : "doughnut",
-        data : chart_data
+        data : chart_data,
+        options : {
+            maintainAspectRatio: false
+        }
     });
 }
